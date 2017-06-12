064fe944e00b3cea6781300056f480c4f26ea79a
emak <emilien.ak@gmail.com>
2017-06-12 19:08:47 +0200

    New font

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,6 +18,7 @@ alias glol="git log --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cg
 alias glola="git log --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all"
 alias glog='git log --oneline --decorate --graph'
 alias gp='git push'
+alias gr='git remote'
 alias grb='git rebase'
 alias grba='git rebase --abort'
 alias grbc='git rebase --continue'
@@ -28,5 +29,4 @@ alias gst='git status'
 alias tnew="tmux new -s"
 alias tach="tmux attach -t"
 
-set PATH $HOME/.rbenv/bin $PATH
-set PATH $HOME/.rbenv/shims $PATH
+set -x EDITOR vim
