6d4be490c5aab3aaba9d967ad6d9d07a4dd50eb8
emak <emilien@happy-dev.fr>
2020-05-27 13:29:20 +0200

    Add new screen layout
    + small fixes

--- .screenlayout/hd.sh
+++ .screenlayout/hd.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+xrandr --output eDP1 --off \
        --output DP1 --off \
        --output DP2 --off \
        --output HDMI1 --off \
-       --output HDMI2 --off \
-       --output VIRTUAL1 --off
+       --output VIRTUAL1 --off \
+       --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 nitrogen --restore &> /dev/null
 xrdb ~/.Xresources
 i3-msg restart &> /dev/null
